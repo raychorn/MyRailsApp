f8e365a427552fc77c5795f7ac9ddd8153083801
Ubuntu <ubuntu@ip-10-139-44-188.ec2.internal>
2013-07-22 23:39:19 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyRailsApp::Application.config.secret_token = 'f418ceab41fa041cf6d855537c5762d539abfc640bb63f7a9a92c9271e752177d37d8ac5f65afaf0eaf54c64bf2832b47e3f61e56eb8604a464570191ba46eae'
